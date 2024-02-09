64d9c53f1461ca2c0467c589d107d9aa51bbbaae
kvn-media <kevin.subagio@gmail.com>
2024-02-09 20:59:21 +0700

    learn prisma orm package

---
+++ lib/dao/user_dao.dart
@@ -0,0 +1,64 @@
+// import 'package:musang_syncronizehub_odyssey/src/generated/prisma/prisma_client.dart';
+
+// createMultipleRecords(PrismaClient prisma) async {
+//   final affectedRows = await prisma.user.createMany(
+//     data: PrismaUnion.$2([
+//       UserCreateManyInput(
+//         userLogin: "musang",
+//         userPass: "musang",
+//         userNicename: "Musang Teknologi Nusantara",
+//         userEmail: "support@musangten.com",
+//         userUrl: null,
+//         userRegistered: DateTime.now(),
+//         userActivationKey: null,
+//         userStatus: null,
+//         displayName: null,
+//       ),
+//       UserCreateManyInput(
+//         userLogin: "admin",
+//         userPass: "admin",
+//         userNicename: "Admin Test",
+//         userEmail: "admin@musangten.com",
+//         userUrl: "",
+//         userRegistered: DateTime.now(),
+//         userActivationKey: null,
+//         userStatus: null,
+//         displayName: null,
+//       ),
+//       UserCreateManyInput(
+//         userLogin: "kevin",
+//         userPass: "kevin",
+//         userNicename: "Kevin",
+//         userEmail: "kevin@musangten.com",
+//         userUrl: null,
+//         userRegistered: DateTime.now(),
+//         userActivationKey: null,
+//         userStatus: null,
+//         displayName: null,
+//       ),
+//       UserCreateManyInput(
+//         userLogin: "syamil",
+//         userPass: "syamil",
+//         userNicename: "Syamil",
+//         userEmail: "syamil@musangten.com",
+//         userUrl: null,
+//         userRegistered: DateTime.now(),
+//         userActivationKey: null,
+//         userStatus: null,
+//         displayName: null,
+//       ),
+//       UserCreateManyInput(
+//         userLogin: "superadmin",
+//         userPass: "superadmin",
+//         userNicename: "Super Admin",
+//         userEmail: "superadmin@musangten.com",
+//         userUrl: null,
+//         userRegistered: DateTime.now(),
+//         userActivationKey: null,
+//         userStatus: null,
+//         displayName: null,
+//       ),
+//     ]),
+//     skipDuplicates: true,
+//   );
+// }
--- lib/features/core/controllers/atg_controller.dart
+++ lib/features/core/controllers/atg_controller.dart
@@ -8,63 +8,12 @@ class ATGBusinessLogic {
   late List<StackedBarSeries<ATGModel, String>> detailedChartData;
 
   // final List<StackedBarSeries<ATGSummary, String>> summaryChartData = [];
-  // final DriftPostgresDatabase database;
-
-  // ATGBusinessLogic(this.database) {
-  //   initializeChartData();
-  // }
-
-  // Future<void> insertData() async {
-  //   var atgs = [
-  //     ATGsCompanion(
-  //       atgTimestamp: Value(DateTime.now().millisecondsSinceEpoch),
-  //       levelBarrel: Value(1000.0),
-  //       volumeChangeBarrel: Value(0.0),
-  //       avgTempCelcius: Value(20.0),
-  //       waterLevelMeter: Value(0.1),
-  //       productTempCelcius: Value(0.0),
-  //       alarm: Value('None'),
-  //       siteId: Value(1),
-  //     ),
-  //   ];
-
-  //   await createMultipleATGs(atgs);
-  // }
-
-  // Create
-  // Future<void> createATG(ATGsCompanion atg) async {
-  //   await database.into(database.aTGs).insert(atg);
-  // }
-
-  // // Create Multiple
-  // Future<void> createMultipleATGs(List<ATGsCompanion> insertData) async {
-  //   for (var atg in insertData) {
-  //     await database.into(database.aTGs).insert(atg);
-  //   }
-  // }
-
-  // // Read
-  // Future<List<ATG>> getAllATGs() async {
-  //   return await database.select(database.aTGs).get();
-  // }
-
-  // // Update
-  // Future<void> updateATG(ATG atg) async {
-  //   await database.update(database.aTGs).replace(atg);
-  // }
-
-  // // Delete
-  // Future<void> deleteATG(ATG atg) async {
-  //   await database.delete(database.aTGs).delete(atg);
-  // }
 
   void initializeChartData() {
     updateChartData();
   }
 
   Future<void> updateData() async {
-    // List<ATG> atgs = await getAllATGs();
-    // listData = atgs.map((atg) => ATGModel.fromATG(atg)).toList();
     updateChartData();
   }
 
